29a0f02714d8144c3bb069e3c9b6881fc4748b7d
Anastasiia <kvasovets.pn@ucu.edu.ua>
2026-03-19 17:37:39 +0200

    1st task solved

---
+++ Convert a linked list to a string/Convert a linked list to a string.py
@@ -0,0 +1,24 @@
+"""
+Module docstring for Convert a linked list to a string
+"""
+
+class Node():
+    def __init__(self, data, next = None):
+        self.data = data
+        self.next = next
+
+def stringify(node):
+    """
+    Module docstring for function stringify
+    """
+
+    if not node:
+        return 'None'
+    nodes_str = []
+    while node:
+        nodes_str.append(str(node.data))
+        node = node.next
+    return ' -> '.join(nodes_str) + ' -> None'
+
+example1 = None
+print(stringify(example1))
