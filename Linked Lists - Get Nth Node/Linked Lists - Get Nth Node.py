bbb7d29c9fd6d7190e48a8131fdadb52475e5bb5
Anastasiia <kvasovets.pn@ucu.edu.ua>
2026-03-20 17:00:26 +0200

    4th task is done

---
+++ Linked Lists - Get Nth Node/Linked Lists - Get Nth Node.py
@@ -0,0 +1,26 @@
+from preloaded import Node
+
+# class Node(object):
+#     """Node class for reference"""
+#     def __init__(self, data, next=None):
+#         self.data = data
+#         self.next = next
+
+
+def get_nth(node, index):
+    """
+    Module docstring for function get_nth
+    """
+
+
+    c = 0
+    while node:
+        if c == index:
+            return node
+        node = node.next
+        c += 1
+
+    raise Exception
+
+print(get_nth('1 -> 2 -> 3 -> null', 0).data)
+print(get_nth('1 -> 2 -> 3 -> null', 1).data)
---
+++ Linked Lists - Get Nth Node/preloaded.py
@@ -0,0 +1,5 @@
+class Node(object):
+    """Node class for reference"""
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
